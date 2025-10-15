086d3c9de4cd6f95d90dac9de7b5727a13ae042a
Rejan Mamugay <rejanmamugay35@gmail.com>
2025-10-16 01:45:39 +0800

    commit

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator, View } from "react-native";
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
+}
